a14b59a2dd5243d8599bbf62ec3f15d836fa7495
DELL <DELL@Jagath>
2025-08-19 16:55:23 +0530

    initial commit

---
+++ javaobj/src/javaobj/Objprimitive.java
@@ -0,0 +1,36 @@
+package javaobj;
+
+public class Objprimitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte b=10;
+		short s=20;
+		int i=30;
+		long l=40;
+		float f=50.0f;
+		double d=60.0d;
+		
+		char c='a';
+		boolean b2=true;
+		
+		//autoboxing =converting primitive data type into object
+		Byte byteobj=b;
+		Short shortobj=s;
+		Integer intobj=i;
+		Long longobj=l;
+		Float floatobj=f;
+		Double doubleobj=d;
+		Character charobj=c;
+		Boolean boolobj=b2;
+		
+		//printing objects
+		System.out.println("the ");
+		
+		
+		
+		
+	}
+
+}
